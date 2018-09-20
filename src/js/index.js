37991309be4a4e149b472002445925ae86c54eab
Truman Blanchard <truman.blanchard@gmail.com>
2018-09-20 10:46:58 -0600

    refactor: simplify js by generalization
    * split this into more atomic commits (time permitting)

--- src/js/index.js
+++ src/js/index.js
@@ -1,47 +1,69 @@
-const rgbR = $(".rgb-r");
+$('article').children('section').each( (index, element) => {
 
-const rgbG = $(".rgb-g");
+	const color = {
+		type: $(element).children('label').html().toLowerCase(),
+		inputs: $(element).children('input'),
+		output: {
+			color: $(element).children('figure').children('span'),
+			caption: $(element) .children('figure').children('figcaption').children('code'),
+		},
+		reset: {
+			button: $(element).children('button'),
+			value: 0,
+			stringValue: 'rgb(0,0,0)',
+		}
+	};
 
-const rgbB = $(".rgb-b");
+	/* event handlers for changing input */
+	color.inputs.change( event => {
+		const newColorValue = $(event.target).val();
+		color.inputs.val(newColorValue);
 
-const rgbCumulative = $(".rgb-rgb");
+		let rgbString = '';
+		switch( color.type ) {
+			case 'r':
+				rgbString = `rgb(${newColorValue}, 0, 0)`;
+			break;
+			case 'g':
+				rgbString = `rgb( 0, ${newColorValue}, 0 )`;
+			break;
+			case 'b':
+				rgbString = `rgb( 0, 0, ${newColorValue})`;
+			break;
+			default:
+				rgbString = `rgb(0,0,0)`;
+			break;
+		}
+			color.output.color.css('background-color', rgbString);
+			color.output.caption.html( rgbString );
+			updateRgbDisplay();
+	});
 
-rgbR.children('input').each( (index, element) => {
-	$(element).change(event => {
-		const rValue = $(event.target).val();
-		const gValue = rgbG.children('.rgb-slider-g').val();
-		const bValue = rgbB.children('.rgb-slider-b').val();
-		rgbR.children('input').each( (index, element) => {
-			$(element).val(rValue);
-			$(element).css('background-color', `rgb( ${rValue}, 0, 0)`);
-			rgbCumulative.css('background-color', `rgb( ${rValue}, ${gValue}, ${bValue})`);
-		});
+	/* event listener for reset button clicks */
+	color.reset.button.click( event => {
+		color.inputs.val( color.reset.value);
+		color.output.color.css('background-color', color.reset.stringValue);
+		color.output.caption.html( color.reset.stringValue );
+		updateRgbDisplay();
 	});
 });
 
-rgbG.children('input').each( (index, element) => {
-	$(element).change(event => {
-		const rValue = rgbR.children('.rgb-slider-r').val();
-		const gValue = $(event.target).val();
-		const bValue = rgbB.children('.rgb-slider-b').val();
-		rgbG.children('input').each( (index, element) => {
-			$(element).val(gValue);
-			$(element).css('background-color', `rgb( 0, ${gValue}, 0)`);
-			rgbCumulative.css('background-color', `rgb( ${rValue}, ${gValue}, ${bValue})`);
-		});
-	});
-});
+/* Event listener for reset all button click */
+$('.rgb--rgb').children('button').click( event => {
+	$('section').children('input').val(0);
+	$('section').children('figure').children('span').css('background-color', 'rgb(0,0,0)');
+	$('section').children('figure').children('figcaption').children('code').html( 'rgb(0, 0, 0)');
+})
 
-rgbB.children('input').each( (index, element) => {
-	$(element).change(event => {
-		const rValue = rgbR.children('.rgb-slider-r').val();
-		const gValue = rgbG.children('.rgb-slider-g').val();
-		const bValue = $(event.target).val();
-		rgbB.children('input').each( (index, element) => {
-			$(element).val(bValue);
-			$(element).css('background-color', `rgb( 0, 0, ${bValue})`);
-			rgbCumulative.css('background-color', `rgb( 0, 0, ${bValue})`);
-			rgbCumulative.css('background-color', `rgb( ${rValue}, ${gValue}, ${bValue})`);
-		});
-	});
-});
+/*
+	Resets all inputs and their corresponding displays to rbg(0,0,0);
+*/
+const updateRgbDisplay = () => {
+		const rValue = $('.rgb--r').children('.rgb-slider--r').val();
+		const gValue = $('.rgb--g').children('.rgb-slider--g').val();
+		const bValue = $('.rgb--b').children('.rgb-slider--b').val();
+		const newRgbValue = `rgb( ${rValue}, ${gValue}, ${bValue})`;
+
+		$('.rgb--rgb').children('figure').children('span').css('background-color', newRgbValue);
+		$('.rgb--rgb').children('figure').children('figcaption').children('code').html( newRgbValue );
+}
